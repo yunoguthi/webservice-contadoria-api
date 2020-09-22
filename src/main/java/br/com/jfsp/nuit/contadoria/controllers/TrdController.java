6c9fd95320e1c727710acbea8b052f4bb27bb54c
Rodrigo <yunoguthi@hotmail.com>
2020-09-22 16:13:42 -0300

    SgsBacen

--- src/main/java/br/com/jfsp/nuit/contadoria/controllers/TrdController.java
+++ src/main/java/br/com/jfsp/nuit/contadoria/controllers/TrdController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.jfsp.nuit.contadoria.services.TRService;
+import br.com.jfsp.nuit.contadoria.services.TrdService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/tr")
-public class TRController {
+@RequestMapping("/trd")
+public class TrdController {
 	
 	@Autowired
-	private TRService service;
+	private TrdService service;
 
 	@GetMapping("/importa")
 	public ResponseEntity<?> importaTR() {
--- src/main/java/br/com/jfsp/nuit/contadoria/repository/TrdRepository.java
+++ src/main/java/br/com/jfsp/nuit/contadoria/repository/TrdRepository.java
@@ -7,16 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import br.com.jfsp.nuit.contadoria.models.TR;
+import br.com.jfsp.nuit.contadoria.models.Trd;
 
 @Repository
-public interface TRRepository extends JpaRepository<TR, Long> {
+public interface TrdRepository extends JpaRepository<Trd, Long> {
 	
-	Optional<TR> findByData(Date data);
+	Optional<Trd> findByData(Date data);
 
 	Boolean existsByData(Date data);
 		
-	@Query("select max(data) from TR")
+	@Query("select max(data) from Trd")
 	Date findMaxData();
 
 }
