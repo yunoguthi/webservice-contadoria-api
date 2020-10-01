c14e769df01be55e393cab091ea647baf5662115
Rodrigo <yunoguthi@hotmail.com>
2020-10-01 20:12:18 -0300

    cron

--- src/main/java/br/com/jfsp/nuit/contadoria/SpringBootSecurityJwtApplication.java
+++ src/main/java/br/com/jfsp/nuit/contadoria/SpringBootSecurityJwtApplication.java
@@ -2,8 +2,10 @@ package br.com.jfsp.nuit.contadoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootSecurityJwtApplication {
 
 	public static void main(String[] args) {
