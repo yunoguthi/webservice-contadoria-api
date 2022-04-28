a4a3a2ef0fbc3d89fd418f3590906dc744e99ed8
Rodrigo Gonçalves Yunoguthi <yunoguthi@hotmail.com>
2022-04-28 11:59:04 -0300

    no-keycloak

--- src/main/java/br/jus/jfsp/nuit/contadoria/config/KeycloakConfig.java
+++ src/main/java/br/jus/jfsp/nuit/contadoria/config/KeycloakConfig.java
@@ -1,15 +1,19 @@
 package br.jus.jfsp.nuit.contadoria.config;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class KeycloakConfig {
-
+/*
     @Bean
     public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
     }
-
+*/
 }
--- src/main/java/br/jus/jfsp/nuit/contadoria/security/SpringSecurityAuditorAware.java
+++ src/main/java/br/jus/jfsp/nuit/contadoria/security/SpringSecurityAuditorAware.java
@@ -3,14 +3,14 @@ package br.jus.jfsp.nuit.predial.security;
 import java.security.Principal;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
+//import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+//import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
+//import org.keycloak.KeycloakSecurityContext;
+//import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
+//import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+//import org.keycloak.representations.AccessToken;
 import java.util.Optional;
 import br.jus.jfsp.nuit.contadoria.util.consts.Consts;
 
@@ -20,12 +20,7 @@ public class SpringSecurityAuditorAware implements AuditorAware<String> {
     private HttpServletRequest request;
 
     public Optional<String> getCurrentAuditor() {
-        try {
-                RefreshableKeycloakSecurityContext context = (RefreshableKeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
-                AccessToken accessToken = context.getToken();
-                String userName = accessToken.getPreferredUsername();
-                return Optional.ofNullable(userName);
-        } catch (Exception e) {}
+
         return Optional.ofNullable(Consts.SERVICO);
     }
 
