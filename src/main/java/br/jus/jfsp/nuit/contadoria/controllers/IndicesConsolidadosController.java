530ef1f4c889dcc04a0e0da81a0d69c8c0ea5383
Rodrigo Gonçalves Yunoguthi <yunoguthi@hotmail.com>
2022-05-10 16:02:56 -0300

    https

--- src/main/java/br/jus/jfsp/nuit/contadoria/controllers/IndicesConsolidadosController.java
+++ src/main/java/br/jus/jfsp/nuit/contadoria/controllers/IndicesConsolidadosController.java
@@ -21,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -127,7 +128,7 @@ public class IndicesConsolidadosController {
 
 	@GetMapping("/download")
 	@ResponseBody
-	public ResponseEntity<Resource> downloadFile(@PathVariable String filename) throws IOException {
+	public ResponseEntity<Resource> downloadFile() throws IOException {
 		Resource file = ManipulaArquivo.download("", "consolidados.csv");
 		Path path = file.getFile()
 				.toPath();
---
+++ src/main/java/br/jus/jfsp/nuit/contadoria/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package br.jus.jfsp.nuit.contadoria.security;
+
+//@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .requiresChannel(channel ->
+//                        channel.anyRequest().requiresSecure())
+//                .authorizeRequests(authorize ->
+//                        authorize.anyRequest().permitAll())
+//                .build();
+//    }
+
+}
